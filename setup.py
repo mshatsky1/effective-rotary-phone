edd8622f3aade88a69ed2539c8c53a3264d099e7
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-11-22 14:12:19 +0200

    Add setup.py for package distribution

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""Setup configuration for rotary phone."""
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="rotary-phone",
+    version="0.1.0",
+    author="Your Name",
+    description="A simple CLI tool for rotary phone number dialing simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "click>=8.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "rotary-phone=rotary_phone.cli:main",
+        ],
+    },
+)
+
